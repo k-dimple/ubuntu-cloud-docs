eed7f93986e18fe8cbca17c68a4b754cca429037
Dimple Kuriakose <dimple.kuriakose@canonical.com>
2023-08-01 00:23:23 +0530

    Redirection for OCI how-tos

--- conf.py
+++ conf.py
@@ -93,5 +93,7 @@ html_css_files = [
 # Set up redirects (https://documatt.gitlab.io/sphinx-reredirects/usage.html)
 # For example: "explanation/old-name.html": "../how-to/prettify.html",
 redirects = {
-    "https://canonical-public-cloud.readthedocs-hosted.com/en/latest/oci/": "https://canonical-oci.readthedocs-hosted.com/en/latest/"
+    "oci/index.": "https://canonical-oci.readthedocs-hosted.com/en/latest/",
+    "oci/oci-how-to/create-chiselled-ubuntu-image": "https://canonical-oci.readthedocs-hosted.com/en/latest/oci-how-to/create-chiselled-ubuntu-image/",
+    "oci/oci-how-to/deploy-pro-container-on-pro-kubernetes-cluster.rst": "https://canonical-oci.readthedocs-hosted.com/en/latest/oci-how-to/deploy-pro-container-on-pro-kubernetes-cluster/"
 }
